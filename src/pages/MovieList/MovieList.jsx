1450b7281e0edbcf434e78cede7b3ebefc6814f9
Tien Hoai <nguyentien1668@gmail.com>
2023-09-13 21:03:58 +0700

    upload call api listFilm

--- src/pages/MovieList/MovieList.jsx
+++ src/pages/MovieList/MovieList.jsx
@@ -1,61 +1,57 @@
-import React from 'react'
-import "./MovieList.scss"
+import React, { useEffect, useState } from "react";
+import "./MovieList.scss";
+import { movieService } from "../../services/movie";
 export default function MovieList() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    fetchMovieList();
+  }, []);
+
+  const fetchMovieList = async () => {
+    const result = await movieService.fecthMovieListApi();
+    setMovieList(result.data.content);
+    console.log(result.data.content);
+  };
+
+  const renderMovieList = () => {
+    return movieList.map((element) => {
+      return (
+        <div className="col-12 col-md-6 col-lg-3">
+          <div className="card mb-4">
+            <img
+              className="card-img-top card-img"
+              src={element.hinhAnh}
+              alt="..."
+            />
+            <div style={{ background: "#fdfcf0" }} className="card-body">
+              <h2 style={{ fontSize: 20 }} className="card-title text-center">
+                {element.tenPhim}
+              </h2>
+              <p></p>
+              <button className="btn btn-success ">
+                <i class="fa-regular fa-thumbs-up"></i> Like: {element.danhGia}
+              </button>
+              <button className="btn btn-danger ml-lg-2">Đặt vé</button>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   return (
-    <div className='main'>
-      <div className='title-movie'>
-        <div className='page-title category-title'>
+    <div className="main">
+      <div className="title-movie">
+        <div className="page-title category-title">
           <h1>Phim Đang Chiếu</h1>
         </div>
-        <div className='py-5'>
-          <div className='row'>
-            <div className='col-12 col-md-6 col-lg-3'>
-              <div className="card mb-4">
-                <img className="card-img-top w-100" src="https://cinestar.com.vn/pictures/Cinestar/07-2022/minion.jpg" alt="..." />
-                <div style={{background: "#fdfcf0"}} className="card-body">
-                  <h2 style={{ fontSize: 24 }} className="card-title text-center">ÁC QUỶ MA SƠ II</h2>
-                  <p></p>
-                  <button className="btn btn-success "><i class="fa-regular fa-thumbs-up"></i> Like: 10</button>
-                  <button className="btn btn-danger ml-lg-2">Đặt vé</button>
-                </div>
-              </div>
-            </div>
-            <div className='col-12 col-md-6 col-lg-3'>
-              <div className="card mb-4">
-                <img className="card-img-top w-100" src="https://cinestar.com.vn/pictures/Cinestar/07-2022/minion.jpg" alt="..." />
-                <div style={{background: "#fdfcf0"}} className="card-body">
-                  <h2 style={{ fontSize: 24 }} className="card-title text-center">ÁC QUỶ MA SƠ II</h2>
-                  <p></p>
-                  <button className="btn btn-success "><i class="fa-regular fa-thumbs-up"></i> Like: 10</button>
-                  <button className="btn btn-danger ml-lg-2">Đặt vé</button>
-                </div>
-              </div>
-            </div>
-            <div className='col-12 col-md-6 col-lg-3'>
-              <div className="card mb-4">
-                <img className="card-img-top w-100" src="https://cinestar.com.vn/pictures/Cinestar/07-2022/minion.jpg" alt="..." />
-                <div style={{background: "#fdfcf0"}} className="card-body">
-                  <h2 style={{ fontSize: 24 }} className="card-title text-center">ÁC QUỶ MA SƠ II</h2>
-                  <p></p>
-                  <button className="btn btn-success "><i class="fa-regular fa-thumbs-up"></i> Like: 10</button>
-                  <button className="btn btn-danger ml-lg-2">Đặt vé</button>
-                </div>
-              </div>
-            </div>
-            <div className='col-12 col-md-6 col-lg-3'>
-              <div className="card mb-4">
-                <img className="card-img-top w-100" src="https://cinestar.com.vn/pictures/Cinestar/07-2022/minion.jpg" alt="..." />
-                <div style={{background: "#fdfcf0"}} className="card-body">
-                  <h2 style={{ fontSize: 24 }} className="card-title text-center">ÁC QUỶ MA SƠ II</h2>
-                  <p></p>
-                  <button className="btn btn-success "><i class="fa-regular fa-thumbs-up"></i> Like: 10</button>
-                  <button className="btn-buyticket btn btn-danger ml-lg-2">Đặt vé</button>
-                </div>
-              </div>
-            </div>
+        <div className="py-5">
+          <div className="row">
+            {renderMovieList()}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
