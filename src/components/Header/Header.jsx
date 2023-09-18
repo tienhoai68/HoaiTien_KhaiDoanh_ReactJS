595c152bf9dec444f94c8a18fd7545bf8c2d5c35
tienhoai68 <nguyentien1668@gmail.com>
2023-09-18 17:51:09 +0700

    upload header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,138 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
 export default function Header() {
   return (
-    <div className="mtw_banner_top" >
-      <div className="mtw_banner_top_content container">
-        <a href="https://advserver.cgv.vn/www/delivery/ck.php?oaparams=2__bannerid=71__zoneid=2__cb=19d2e0e33e__oadest=https%3A%2F%2Fwww.cgv.vn%2Fdefault%2Fnewsoffer%2Fcgv-zalo-092023%2F" target="_blank" >
-          <img className='w-100' src="./img/zalopay.jpg" alt="zalo" />
-        </a>
-      </div>
-      <div className="header-language-background container">
-        <div className="header-container">
-          <div className="header-language-right">
-            <div className='header-career mn-newsoffer'>
-              <p><a href="">Tin mới & Ưu đãi</a></p>
-            </div>
-            <div className='header-career mn-myticket'>
-              <p><a href="">Vé Của Tôi</a></p>
-            </div>
-            <div className='header-top-account'>
-              <div className='account-header-wrapper mn-login'>
-                  <a href="" className='topskip-link skip-account'>
-                    <span className='label'>Đăng nhập/ Đăng ký</span>
-                  </a>
+    // <div className="mtw_banner_top" >
+    //   <div className="mtw_banner_top_content container">
+    //     <a href="https://advserver.cgv.vn/www/delivery/ck.php?oaparams=2__bannerid=71__zoneid=2__cb=19d2e0e33e__oadest=https%3A%2F%2Fwww.cgv.vn%2Fdefault%2Fnewsoffer%2Fcgv-zalo-092023%2F" target="_blank" >
+    //       <img className='w-100' src="./img/zalopay.jpg" alt="zalo" />
+    //     </a>
+    //   </div>
+    //   <div className="header-language-background container">
+    //     <div className="header-container">
+    //       <div className="header-language-right">
+    //         <div className='header-career mn-newsoffer'>
+    //           <p><a href="">Tin mới & Ưu đãi</a></p>
+    //         </div>
+    //         <div className='header-career mn-myticket'>
+    //           <p><a href="">Vé Của Tôi</a></p>
+    //         </div>
+    //         <div className='header-top-account'>
+    //           <div className='account-header-wrapper mn-login'>
+    //             <a href="" className='topskip-link skip-account'>
+    //               <span className='label'>Đăng nhập/ Đăng ký</span>
+    //             </a>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   {/* ----------Start-Navbar--------  */}
+    //   {/* <Navbar /> */}
+    //   {/* ----------End-Navbar--------  */}
+    //   <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light container" id="ftco-navbar">
+    //     <div className="container">
+    //       <a className="navbar-brand" href="index.html">Jobpply</a>
+    //       <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+    //         <span className="oi oi-menu" /> Menu
+    //       </button>
+    //       <div className="navbar-collapse collapse" id="ftco-nav" style={{}}>
+    //         <ul className="navbar-nav ml-auto">
+    //           <li className="nav-item active"><a href="index.html" className="nav-link">Home</a></li>
+    //           <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
+    //           <li className="nav-item"><a href="candidates.html" className="nav-link">Canditates</a></li>
+    //           <li className="nav-item"><a href="blog.html" className="nav-link">Blog</a></li>
+    //           <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
+    //           <li className="nav-item cta mr-md-1"><a href="new-post.html" className="nav-link">Post a Job</a></li>
+    //           <li className="nav-item cta cta-colored"><a href="job-post.html" className="nav-link">Want a Job</a></li>
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   </nav>
+    // </div>
+    <header className="header_area">
+      <div className="sub_header">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-md-4 col-xl-6">
+              <div id="logo">
+                <a href="index.html"><img src="img/Logo.png" alt title /></a>
               </div>
-            </div>        
+            </div>
+            <div className="col-md-8 col-xl-6">
+              <div className="sub_header_social_icon float-right">
+                <a href="#"><i className="flaticon-phone" />+02 213 - 256 (365)</a>
+                <a href="#" className="register_icon"><i className="ti-arrow-right" />REGISTER</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
- {/* ----------Start-Navbar--------  */}
-      <Navbar/>
- {/* ----------End-Navbar--------  */}
-    </div>
+      <div className="main_menu">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <nav className="navbar navbar-expand-lg navbar-light">
+                <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon" />
+                </button>
+                <div className="navbar-collapse collapse" id="navbarSupportedContent" style={{}}>
+                  <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                      <a className="nav-link active" href="index.html">Home</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="about.html" className="nav-link">About us</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="team.html" className="nav-link">team</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="gallery.html" className="nav-link">gallery</a>
+                    </li>
+                    <li className="nav-item dropdown show">
+                      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        Pages
+                      </a>
+                      <div className="dropdown-menu show" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="elements.html">Elements</a>
+                        <a className="dropdown-item" href="single-blog.html">Single blog</a>
+                      </div>
+                    </li>
+                    <li className="nav-item">
+                      <a href="blog.html" className="nav-link">blog</a>
+                    </li>
+                    <li className="nav-item">
+                      <a href="contact.html" className="nav-link">Contact</a>
+                    </li>
+                  </ul>
+                  <div className="header_social_icon d-none d-lg-block">
+                    <ul>
+                      <li><a href="#"><i className="ti-facebook" /></a></li>
+                      <li>
+                        <a href="#"> <i className="ti-twitter" /></a>
+                      </li>
+                      <li><a href="#"><i className="ti-instagram" /></a></li>
+                      <li><a href="#"><i className="ti-skype" /></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+              <div className="header_social_icon d-block d-lg-none">
+                <ul>
+                  <li><a href="#"><i className="ti-facebook" /></a></li>
+                  <li>
+                    <a href="#"> <i className="ti-twitter" /></a>
+                  </li>
+                  <li><a href="#"><i className="ti-instagram" /></a></li>
+                  <li><a href="#"><i className="ti-skype" /></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
   )
 }
--- src/components/Header/components/Navbar.jsx
+++ src/components/Header/components/Navbar.jsx
@@ -1,89 +1,112 @@
-import React from 'react'
 import "./navbar.scss"
 import "./responsiveNavbar.scss"
+import { useNavigate, NavLink } from 'react-router-dom'
 export default function Navbar() {
-    return (
-        <div className='page-header'>
-            <div className='page-header-container container'>
-                <nav  className="navbar navbar-expand-lg navbar-light p-lg-0">
-                    <a className="navbar-brand logo" href="#">
-                    <img src="././img/cgvlogo.png" alt="logo" />
-                    </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon" />
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                        <ul className="navbar-nav">
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropMovieLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Phim
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropMovieLink">
-                                    <a className="dropdown-item" href="#">Phim Đang Chiếu</a>
-                                    <a className="dropdown-item" href="#">Phim Sắp Chiếu</a>
-                                </div>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Rạp CGV
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <a className="dropdown-item" href="#">Tất Cả Các Rạp</a>
-                                    <a className="dropdown-item" href="#">Rạp Đặc Biệt</a>
-                                    <a className="dropdown-item" href="#">Rạp 3D</a>
-                                </div>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Thành viên
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <a className="dropdown-item" href="#">Tài khoản CGV</a>
-                                    <a className="dropdown-item" href="#">Quyền lợi</a>
-                                </div>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Cultureplex
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <a className="dropdown-item" href="#">Quầy Online</a>
-                                    <a className="dropdown-item" href="#">Thuê Rạp & Vé Nhóm</a>
-                                    <a className="dropdown-item" href="#">e-CGV</a>
-                                    <a className="dropdown-item" href="#">CGV eGift</a>
-                                    <a className="dropdown-item" href="#">CGV Rules</a>
-                                </div>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Tuyển dụng
-                                    <img className='hot-job' src="././img/hot-jobs.png" alt="hot-job" />
-                                </a>
-                                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <a className="dropdown-item" href="#">Tuyển dụng</a>
-                                    <a className="dropdown-item" href="#">Khối Văn Phòng</a>
-                                    <a className="dropdown-item" href="#">Khối Cụm Rạp</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className='header-search'>
-                        <div className='header-search-left'>
-                            <p className='kenhcine'>
-                                <img src="././img/kenhcine.gif" alt="kên cine" />
-                            </p>
-                        </div>
-                        <div className='header-search-right'>
-                            <p>
-                                <a href="">
-                                    <img src="././img/mua-ve_ngay.png" alt="mua-ve" />
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-        </div>
 
-    )
+    // return (
+    //     <div className='page-header'>
+    //         <div className='page-header-container container'>
+    //             <nav className="navbar navbar-expand-lg navbar-light p-lg-0">
+    //                 <a className="navbar-brand logo" href="#">
+    //                     <img src="././img/cgvlogo.png" alt="logo" />
+    //                 </a>
+    //                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    //                     <span className="navbar-toggler-icon" />
+    //                 </button>
+    //                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    //                     <ul className="navbar-nav">
+    //                         <li className="nav-item dropdown">
+    //                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropMovieLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //                                 Phim
+    //                             </a>
+    //                             <div className="dropdown-menu" aria-labelledby="navbarDropMovieLink">
+    //                                 <NavLink className="dropdown-item" to="/movie-list">Phim Đang Chiếu</NavLink>
+    //                                 <a className="dropdown-item" href="#">Phim Sắp Chiếu</a>
+    //                             </div>
+    //                         </li>
+    //                         <li className="nav-item dropdown">
+    //                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //                                 Rạp CGV
+    //                             </a>
+    //                             <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    //                                 <a className="dropdown-item" href="#">Tất Cả Các Rạp</a>
+    //                                 <a className="dropdown-item" href="#">Rạp Đặc Biệt</a>
+    //                                 <a className="dropdown-item" href="#">Rạp 3D</a>
+    //                             </div>
+    //                         </li>
+    //                         <li className="nav-item dropdown">
+    //                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //                                 Thành viên
+    //                             </a>
+    //                             <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    //                                 <a className="dropdown-item" href="#">Tài khoản CGV</a>
+    //                                 <a className="dropdown-item" href="#">Quyền lợi</a>
+    //                             </div>
+    //                         </li>
+    //                         <li className="nav-item dropdown">
+    //                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //                                 Cultureplex
+    //                             </a>
+    //                             <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    //                                 <a className="dropdown-item" href="#">Quầy Online</a>
+    //                                 <a className="dropdown-item" href="#">Thuê Rạp & Vé Nhóm</a>
+    //                                 <a className="dropdown-item" href="#">e-CGV</a>
+    //                                 <a className="dropdown-item" href="#">CGV eGift</a>
+    //                                 <a className="dropdown-item" href="#">CGV Rules</a>
+    //                             </div>
+    //                         </li>
+    //                         <li className="nav-item dropdown">
+    //                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //                                 Tuyển dụng
+    //                                 <img className='hot-job' src="././img/hot-jobs.png" alt="hot-job" />
+    //                             </a>
+    //                             <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    //                                 <a className="dropdown-item" href="#">Tuyển dụng</a>
+    //                                 <a className="dropdown-item" href="#">Khối Văn Phòng</a>
+    //                                 <a className="dropdown-item" href="#">Khối Cụm Rạp</a>
+    //                             </div>
+    //                         </li>
+    //                     </ul>
+    //                 </div>
+    //                 <div className='header-search'>
+    //                     <div className='header-search-left'>
+    //                         <p className='kenhcine'>
+    //                             <img src="././img/kenhcine.gif" alt="kên cine" />
+    //                         </p>
+    //                     </div>
+    //                     <div className='header-search-right'>
+    //                         <p>
+    //                             <a href="">
+    //                                 <img src="././img/mua-ve_ngay.png" alt="mua-ve" />
+    //                             </a>
+    //                         </p>
+    //                     </div>
+    //                 </div>
+    //             </nav>
+    //             {/* <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    //                 <div className="container">
+    //                     <a className="navbar-brand" href="index.html">Jobpply</a>
+    //                     <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+    //                         <span className="oi oi-menu" /> Menu
+    //                     </button>
+    //                     <div className="navbar-collapse collapse" id="ftco-nav" style={{}}>
+    //                         <ul className="navbar-nav ml-auto">
+    //                             <li className="nav-item active"><a href="index.html" className="nav-link">Home</a></li>
+    //                             <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
+    //                             <li className="nav-item"><a href="candidates.html" className="nav-link">Canditates</a></li>
+    //                             <li className="nav-item"><a href="blog.html" className="nav-link">Blog</a></li>
+    //                             <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
+    //                             <li className="nav-item cta mr-md-1"><a href="new-post.html" className="nav-link">Post a Job</a></li>
+    //                             <li className="nav-item cta cta-colored"><a href="job-post.html" className="nav-link">Want a Job</a></li>
+    //                         </ul>
+    //                     </div>
+    //                 </div>
+    //             </nav> */}
+
+
+
+    //         </div>
+    //     </div>
+
+    // )
 }
