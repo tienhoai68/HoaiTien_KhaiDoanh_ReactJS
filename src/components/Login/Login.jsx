755aa67cd1ca9e81c81d82ab56efc0e94459269d
Tien Hoai <nguyentien1668@gmail.com>
2023-09-23 10:37:01 +0700

    upload logout button

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,126 +1,103 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // import "./style.css";
 import "./Login.scss";
-import Register from './components/Register/Register';
-import { userService } from '../../services/user';
-import { useDispatch } from "react-redux"
-import { setUserInfoAction } from '../../store/actions/userAction';
+import Register from "./components/Register/Register";
+import { userService } from "../../services/user";
+import { useDispatch } from "react-redux";
+import { setUserInfoAction } from "../../store/actions/userAction";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [stateLogin, setStateLogin] = useState({
+    taiKhoan: "",
+    matKhau: "",
+  });
 
-    const [stateLogin, setStateLogin] = useState({
-        taiKhoan: "",
-        matKhau: "",
+  const handleClickRegister = () => {
+    const container = document.getElementById("container");
+    container.classList.add("right-panel-active");
+  };
+  const handleClickLogin = () => {
+    const container = document.getElementById("container");
+    container.classList.remove("right-panel-active");
+  };
+
+  const handleChangeLogin = (event) => {
+    setStateLogin({
+      ...stateLogin,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const handleClickRegister = () => {
-        const container = document.getElementById("container");
-        container.classList.add("right-panel-active");
+  const handleSubmitLogin = async (event) => {
+    event.preventDefault();
+    const result = await userService.loginApi(stateLogin);
+    console.log(result);
+    dispatch(setUserInfoAction(result.data.content));
+    localStorage.setItem("USER_INFO", JSON.stringify(result.data.content));
+    navigate("/");
+  };
 
-    };
-    const handleClickLogin = () => {
-        const container = document.getElementById("container");
-        container.classList.remove("right-panel-active");
-    };
-
-    const handleChangeLogin = (event) => {
-        setStateLogin({
-            ...stateLogin,
-            [event.target.name]: event.target.value,
-        })
-    }
-
-    const handleSubmitLogin = async (event) => {
-        event.preventDefault();
-        const result = await userService.loginApi(stateLogin);
-        console.log(result);
-        dispatch(setUserInfoAction(result.data.content));
-    }
-
-
-
-    return (
-        <div className='background-login'>
-            <div className='container' id='container'>
-                <div className='form-container register-container'>
-                    {/* <form onSubmit={handleSubmitRegister}>
-                        <h1>Register here</h1>
-                        <div className='form-control'>
-                            <input onChange={handleChangeRegister} type="text" placeholder='Tài khoản' />
-                            <span></span>
-                        </div>
-                        <div className='form-control'>
-                            <input onChange={handleChangeRegister} type="text" placeholder='Mật khẩu' />
-                            <span></span>
-                        </div>
-                        <div className='form-control'>
-                            <input onChange={handleChangeRegister} type="text" placeholder='Email' />
-                            <span></span>
-                        </div>
-                        <div className='form-control'>
-                            <input onChange={handleChangeRegister} type="text" placeholder='Số điện thoại' />
-                            <span></span>
-                        </div>
-                        <div className='form-control'>
-                            <input onChange={handleChangeRegister} type="text" placeholder='Mã nhóm' />
-                            <span></span>
-                        </div>
-                        <div className='form-control'>
-                            <input onChange={handleChangeRegister} type="text" placeholder='Họ Tên' />
-                            <span></span>
-                        </div>
-                        <button type='submit'>Register</button>
-                    </form> */}
-                    <Register />
-                </div>
-                <div className='form-container login-container'>
-                    <form onSubmit={handleSubmitLogin} className='form-lg'>
-                        <h1>Login here</h1>
-                        <div className='form-control2'>
-                            <input onChange={handleChangeLogin} name='taiKhoan' type="text" placeholder='Tài khoản' />
-                            <span></span>
-                        </div>
-                        <div className='form-control2'>
-                            <input onChange={handleChangeLogin} name='matKhau' type="text" placeholder='Mật Khẩu' />
-                            <span></span>
-                        </div>
-                        <button type='submit'>Login</button>
-                    </form>
-                </div>
-                <div className='overlay-container'>
-                    <div className='overlay'>
-                        <div className='overlay-panel over-left'>
-                            <h1 className='title'>
-                                Hello
-                            </h1>
-                            <p>Nếu bạn có tài khoản, hãy đăng nhập</p>
-                            <button onClick={handleClickLogin} className='ghost' id='login'>
-                                Login
-                                <i className="fa-solid fa-arrow-left" />
-                            </button>
-                        </div>
-
-                        <div className='overlay-panel overlay-right'>
-                            <h1 className='title'>
-                                Hello
-                            </h1>
-                            <p>Nếu bạn không có tài khoản, hãy đăng ký</p>
-                            <button onClick={handleClickRegister} className='ghost' id='register'>
-                                Register
-                                <i className="fa-solid fa-arrow-right" />
-                            </button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="background-login">
+      <div className="container" id="container">
+        <div className="form-container register-container">
+          {/* cắt thành component Register */}
+          <Register />
+        </div>
+        <div className="form-container login-container">
+          <form onSubmit={handleSubmitLogin} className="form-lg">
+            <h1>Login here</h1>
+            <div className="form-control2">
+              <input
+                onChange={handleChangeLogin}
+                name="taiKhoan"
+                type="text"
+                placeholder="Tài khoản"
+              />
+              <span></span>
+            </div>
+            <div className="form-control2">
+              <input
+                onChange={handleChangeLogin}
+                name="matKhau"
+                type="text"
+                placeholder="Mật Khẩu"
+              />
+              <span></span>
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel over-left">
+              <h1 className="title">Hello</h1>
+              <p>Nếu bạn có tài khoản, hãy đăng nhập</p>
+              <button onClick={handleClickLogin} className="ghost" id="login">
+                Login
+                <i className="fa-solid fa-arrow-left" />
+              </button>
             </div>
 
+            <div className="overlay-panel overlay-right">
+              <h1 className="title">Hello</h1>
+              <p>Nếu bạn không có tài khoản, hãy đăng ký</p>
+              <button
+                onClick={handleClickRegister}
+                className="ghost"
+                id="register"
+              >
+                Register
+                <i className="fa-solid fa-arrow-right" />
+              </button>
+            </div>
+          </div>
         </div>
-
-
-
-
-
-    )
+      </div>
+    </div>
+  );
 }
--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -3,18 +3,20 @@ import { SET_USER_INFO } from "../types/userType";
 const DEDAULT_STATE = {
   userInfo: null,
 };
+const stringify = localStorage.getItem("USER_INFO");
+// console.log(stringify);
+if (stringify) {
+  DEDAULT_STATE.userInfo = JSON.parse(stringify);
+}
+// console.log(DEDAULT_STATE.userInfo);
 
 export const userReducer = (state = DEDAULT_STATE, action) => {
 
-  switch (action.payload) {
+  switch (action.type) {
     case SET_USER_INFO: {
       state.userInfo = action.payload;
+      break;
     }
-
-      break;
-
-    default:
-      break;
   }
 
   return { ...state };
