4e289651c10071a0612d74432f67dabf45be3e48
tienhoai68 <nguyentien1668@gmail.com>
2023-09-29 19:54:42 +0700

    update validate register

--- src/components/Login/components/Register/Register.jsx
+++ src/components/Login/components/Register/Register.jsx
@@ -1,60 +1,68 @@
-import React, { useState } from 'react'
-import { userService } from '../../../../services/user';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  taiKhoan: Yup.string().required('(*) Tài khoản không được để trống'),
+  matKhau: Yup.string().required('(*) Mật khẩu không được để trống').min(8, 'Mật khẩu phải có ít nhất 8 ký tự'),
+  email: Yup.string().email('(*) Email không hợp lệ').required('(*) Email không được để trống'),
+  soDt: Yup.string().required('(*) Số điện thoại không được để trống'),
+  hoTen: Yup.string().required('(*) Họ tên không được để trống'),
+});
 
 export default function Register() {
 
-  const [stateRegister, setStateRegister] = useState({
-    taiKhoan: "",
-    matKhau: "",
-    email: "",
-    soDt: "",
-    maNhom: "GP08",
-    hoTen: ""
-  });
 
-  const handleChangeRegister = (event) => {
-    setStateRegister({
-      ...stateRegister,
-      [event.target.name]: event.target.value,
-    })
-  }
-
-  const handleSubmitRegister = async (event) => {
-    event.preventDefault();
-    console.log(stateRegister);
-    await userService.registerApi(stateRegister);
-  }
-  // console.log(stateRegister);
-
+  const handleSubmitRegister = async (values, { resetForm }) => {
+    console.log(values);
+    // Gọi hàm xử lý đăng ký ở đây
+    resetForm();
+  };
 
   return (
-    <form onSubmit={handleSubmitRegister}>
+    <div>
       <h1>Register here</h1>
-      <div className='form-control'>
-        <input name='taiKhoan' onChange={handleChangeRegister} type="text" placeholder='Tài khoản' />
-        <span></span>
-      </div>
-      <div className='form-control'>
-        <input name='matKhau' onChange={handleChangeRegister} type="text" placeholder='Mật khẩu' />
-        <span></span>
-      </div>
-      <div className='form-control'>
-        <input name='email' onChange={handleChangeRegister} type="text" placeholder='Email' />
-        <span></span>
-      </div>
-      <div className='form-control'>
-        <input name='soDt' onChange={handleChangeRegister} type="text" placeholder='Số điện thoại' />
-        <span></span>
-      </div>
-      {/* <div className='form-control'>
-        <input onChange={handleChangeRegister} type="text" placeholder='Mã nhóm' />
-        <span></span>
-      </div> */}
-      <div className='form-control'>
-        <input name='hoTen' onChange={handleChangeRegister} type="text" placeholder='Họ Tên' />
-        <span></span>
-      </div>
-      <button type='submit'>Register</button>
-    </form>
-  )
+      <Formik
+        initialValues={{
+          taiKhoan: '',
+          matKhau: '',
+          email: '',
+          soDt: '',
+          maNhom: 'GP08',
+          hoTen: '',
+        }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmitRegister}
+      >
+        <Form className="register-form">
+          <div className='form-control'>
+            <Field name='taiKhoan' type='text' placeholder='Tài khoản' />
+            <span></span>
+            <ErrorMessage name='taiKhoan' component='label' className='form-label text-danger' />
+          </div>
+          <div className='form-control'>
+            <Field name='matKhau' type='password' placeholder='Mật khẩu' />
+            <span></span>
+            <ErrorMessage name='matKhau' component='label' className='form-label text-danger' />
+          </div>
+          <div className='form-control'>
+            <Field name='email' type='text' placeholder='Email' />
+            <span></span>
+            <ErrorMessage name='email' component='label' className='form-label text-danger' />
+          </div>
+          <div className='form-control'>
+            <Field name='soDt' type='text' placeholder='Số điện thoại' />
+            <span></span>
+            <ErrorMessage name='soDt' component='label' className='form-label text-danger' />
+          </div>
+          <div className='form-control'>
+            <Field name='hoTen' type='text' placeholder='Họ Tên' />
+            <span></span>
+            <ErrorMessage name='hoTen' component='label' className='form-label text-danger' />
+          </div>
+          <button type='submit'>Register</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
